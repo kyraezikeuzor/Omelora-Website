42645fb52e1c059612e441fe209d334bf8ad4f4b
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2023-06-28 22:43:53 -0500

    "Added images file"

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+function navbar() {
+  return (
+    <>
+    <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="images\OmeloraLogo.png"></Link>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default navbar
