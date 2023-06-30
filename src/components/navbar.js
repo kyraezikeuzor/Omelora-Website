694184c288fe6b12f425bfdc34209e3147b0a407
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2023-06-29 23:01:14 -0500

    Added files

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,22 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
-function navbar() {
+
+function Navbar() {
   return (
     <>
-    <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="images\OmeloraLogo.png"></Link>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to="/" className='images\OmeloraLogo.png'>
+            TRVL <i className='public\images\OmeloraLogo.png'/>
+          </Link>
+          <div className="menu-icon">
+            <i className={"icon"}/>
+          </div>
         </div>
-    </nav>
+      </nav>
     </>
-  )
+  );
 }
 
-export default navbar
+export default Navbar
